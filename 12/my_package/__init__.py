e89c04c8e26c9dc10915cbe8a1b7dc74ae74c3bc
vedanshu-1234 <niga.man.19991@gmail.com>
2025-05-05 20:26:36 +0530

    Add files via upload

---
+++ 12/my_package/__init__.py
@@ -0,0 +1,3 @@
+from .list1 import *
+from .set2 import *
+from .dict3 import *
---
+++ 12/my_package/dict3.py
@@ -0,0 +1,14 @@
+mydict={}
+
+def len3():
+    return len(mydict)
+
+def add3(k, v):
+    mydict[k] = v
+    return mydict
+
+def keys3():
+    return mydict.keys()
+
+def values3():
+    return mydict.values()
