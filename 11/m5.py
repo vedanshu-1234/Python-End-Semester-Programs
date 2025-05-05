a17181e0cad778befc6c6ab5c64b7aa1760d3ba9
vedanshu-1234 <niga.man.19991@gmail.com>
2025-05-05 20:23:05 +0530

    Add files via upload

---
+++ 11/m5.py
@@ -0,0 +1,12 @@
+import cal1
+
+x = 10
+y = 2
+
+print("Calculator operations:\n")
+print("Addition: ",cal1.add1(x, y))
+print("Subtraction: ",cal1.sub1(x, y))
+print("Multiplication: ",cal1.mul1(x, y))
+print("Division: ",cal1.div1(x, y))
+print("Modulus: ",cal1.mod1(x, y))
+print("Power: ",cal1.pow1(x, y))
